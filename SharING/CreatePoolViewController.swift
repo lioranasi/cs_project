6ef7b499969c35f2267d46e7c1096cc5e046ef65
sedatyalcin <sedat.yalcin@ggl.hisarschool.k12.tr>
2016-11-06 00:31:01 +0300

    What's in a name? That which we call a rose by any other name would smell as sweet

--- SharING/CreatePoolViewController.swift
+++ SharING/CreatePoolViewController.swift
@@ -11,9 +11,41 @@ import UIKit
 class CreatePoolViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
 
     @IBOutlet weak var collView: UICollectionView!
+    @IBOutlet weak var imgQRCode: UIImageView!
+    
+    let myString = "649354"
+    var qrcodeImage: CIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        if qrcodeImage == nil {
+            let myNSString = myString as NSString
+            
+            //Convert NSString to NSData
+            let data = myNSString.data(using: String.Encoding.utf8.rawValue)!
+            /**
+ let data = myNSString.data(using: String.Encoding.utf8.rawValue)!
+ **/
+            /**
+             let data = textField.text?.data(using: String.Encoding.isoLatin1, allowLossyConversion: false)
+             **/
+            
+            let filter = CIFilter(name: "CIQRCodeGenerator")
+            
+            filter?.setValue(data, forKey: "inputMessage")
+            filter?.setValue("Q", forKey: "inputCorrectionLevel")
+            
+            qrcodeImage = filter?.outputImage
+            
+            displayQRCodeImage()
+            
+            
+        }
+        
+        
+        
+        
         // Do any additional setup after loading the view.
     }
 
@@ -37,6 +69,18 @@ class CreatePoolViewController: UIViewController,UICollectionViewDelegate,UIColl
         return cell
         
     }
+    
+    func displayQRCodeImage() {
+        let scaleX = imgQRCode.frame.size.width / qrcodeImage.extent.size.width
+        let scaleY = imgQRCode.frame.size.height / qrcodeImage.extent.size.height
+        
+        let transformedImage = qrcodeImage.applying(CGAffineTransform(scaleX: scaleX, y: scaleY))
+        
+        imgQRCode.image = UIImage(ciImage: transformedImage)
+        
+    }
+    
+    
     
     /*
     // MARK: - Navigation
