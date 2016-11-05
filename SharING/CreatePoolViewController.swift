d4247591ace158d7e42d59e7bc287ec4c52df0b8
Liora <liora.nasi@ggl.hisarschool.k12.tr>
2016-11-05 21:19:18 +0300

    FOK COMMIT

---
+++ SharING/CreatePoolViewController.swift
@@ -0,0 +1,51 @@
+//
+//  CreatePoolViewController.swift
+//  SharING
+//
+//  Created by Liora Nasi on 05/11/2016.
+//  Copyright © 2016 Liora Nasi. All rights reserved.
+//
+
+import UIKit
+
+class CreatePoolViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
+
+    @IBOutlet weak var collView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell1", for: indexPath)
+        return cell
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
